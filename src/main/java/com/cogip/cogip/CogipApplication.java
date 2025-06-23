840b0bb8345dd9745adeb462aa67194086952235
manu-cj <carenjotje@gmail.com>
2025-06-23 09:13:41 +0200

    init

---
+++ src/main/java/com/cogip/cogip/CogipApplication.java
@@ -0,0 +1,13 @@
+package com.cogip.cogip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CogipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CogipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cogip/cogip/CogipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cogip.cogip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CogipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
